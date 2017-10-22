c17fc333fb57dace8bd6bce09de64ccb2c3b6ce4
Ryan <bradburyrp@gmail.com>
2017-10-22 12:17:13 -0400

    Reads from text file and asks questions

--- input.py
+++ input.py
@@ -1,5 +1,15 @@
 answer = input("In this experiment, we'll be asking you a series of 15 questions related to tech companies. These questions are either 'yes' or 'no'. At the end, the program will try to guess which company you were thinking about. If it's right, you'll tell it so with a 'yes'. If it's wrong, you'll say 'no', and you'll have an opportunity to tell it which company you were thinking of. Is this ok? Y/n: ")
 if (answer == 'Y' or answer == 'y'):
-	print("You said 'yes'.")
+	i = 1
+	with open('questions.txt', 'r') as q:
+		for x in q:
+			ans = input("Line " + str(i) + ": " + x)
+			
+			if (ans == 'Y' or ans == 'y'):
+				print("You replied 'yes' to question " + str(i))
+			elif(ans == 'N' or ans == 'n'):
+				print("You replied 'no' to question " + str(i))
+			
+			i += 1
 elif (answer == 'N' or answer == 'n'):
 	print("You said 'no'.")
