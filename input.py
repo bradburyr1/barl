3ef069543f38577a0d961ef78094991b372eff80
Francisco Salinas <remixielive@live.com>
2017-11-08 16:10:21 -0500

    Made an answer array.

--- input.py
+++ input.py
@@ -1,5 +1,7 @@
 answer = input("In this experiment, we'll be asking you a series of 15 questions related to tech companies. These questions are either 'yes' or 'no'. At the end, the program will try to guess which company you were thinking about. If it's right, you'll tell it so with a 'yes'. If it's wrong, you'll say 'no', and you'll have an opportunity to tell it which company you were thinking of. Is this ok? Y/n: ")
 if (answer == 'Y' or answer == 'y'):
+
+	answerArray = [];
 	i = 1
 	with open('questions.txt', 'r') as q:
 		for x in q:
@@ -10,6 +12,10 @@ if (answer == 'Y' or answer == 'y'):
 			elif(ans == 'N' or ans == 'n'):
 				print("You replied 'no' to question " + str(i))
 			
+			answerArray.append(ans)
+			
+			
 			i += 1
+			
 elif (answer == 'N' or answer == 'n'):
 	print("You said 'no'.")
