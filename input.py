c1db48ffbd3ca0a9b793ad31cdd78193145c783d
Francisco Salinas <remixielive@live.com>
2017-11-12 00:40:46 -0500

    Added support for test data.

--- input.py
+++ input.py
@@ -1,3 +1,6 @@
+import csv
+import os
+   
 answer = input("In this experiment, we'll be asking you a series of 15 questions related to tech companies. These questions are either 'yes' or 'no'. At the end, the program will try to guess which company you were thinking about. If it's right, you'll tell it so with a 'yes'. If it's wrong, you'll say 'no', and you'll have an opportunity to tell it which company you were thinking of. Is this ok? Y/n: ")
 if (answer == 'Y' or answer == 'y'):
 
@@ -5,17 +8,28 @@ if (answer == 'Y' or answer == 'y'):
 	i = 1
 	with open('questions.txt', 'r') as q:
 		for x in q:
-			ans = input("Line " + str(i) + ": " + x)
+			ans = input("\n"+x)
 			
 			if (ans == 'Y' or ans == 'y'):
 				print("You replied 'yes' to question " + str(i))
+				answerArray.append(ans)
 			elif(ans == 'N' or ans == 'n'):
 				print("You replied 'no' to question " + str(i))
-			
-			answerArray.append(ans)
-			
-			
+				answerArray.append(ans)
+				
+
 			i += 1
+	
+	
+	answerArray = [w.replace('y', '1') for w in answerArray]
+	answerArray = [w.replace('n', '0') for w in answerArray]
+
+	#with open('test_data_insert.csv', 'a', newline='\n') as csv_file:
+	with open('test_data_insert.csv', 'w') as csv_file:
+		writer = csv.writer(csv_file)
+		writer.writerow(answerArray)
+		
+	import prediction
 			
 elif (answer == 'N' or answer == 'n'):
 	print("You said 'no'.")
--- prediction.py
+++ prediction.py
@@ -13,6 +13,10 @@ train_target = [0 for x in range(h2)]
 #train target with numbers instead of numbers
 tt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
+#2-dim train data array
+w, h = 15, 1;
+test_data = [[0 for x in range(w)] for y in range(h)]
+
 #Open the CSV file for train data
 with open('training_data_no_labels.csv', 'rt') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -42,11 +46,28 @@ with open('training_targets.csv', 'rt') as csvfile:
 			k += 1
 		i += 1
 		
+#Open the CSV file for test data
+with open('test_data_insert.csv', 'rt') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	i = 0
+	#Loop through the rows of the file
+	for row in reader:
+		k = 0
+		#Loop through the elements of each row, assign to test_data
+		for x in range(len(row)):
+			test_data[i][x] = row[k]
+			#print (test_data[i][x])
+			k += 1
+		i += 1
+		
 #print(train_data)
 #train the svm classifier
 s = svm.SVC(kernel = 'rbf') # 'rbf', 'poly', 'linear', 'sigmoid', etc.
 s.fit(train_data, tt)
 
 #you are ready to predict/test
-decisions = s.predict(train_data)
-print(decisions)
+decisions = s.predict(test_data)
+
+#print(decisions)
+
+print("\n Prediction: " + str(train_target[decisions[0]-1]))
